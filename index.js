b2ce6789a981ae466f123e32ec0ec694c915a0fe
ijlal khanzada <ijlalkhanzada@gmail.com>
2024-05-30 13:04:36 +0500

    Html Template

--- index.js
+++ index.js
@@ -2,12 +2,19 @@ const dialogflow = require('@google-cloud/dialogflow');
 const { WebhookClient, Suggestion } = require('dialogflow-fulfillment');
 const express = require("express")
 const cors = require("cors");
-
+const path = require('path');
 const app = express();
 app.use(express.json())
 app.use(cors());
-
 const PORT = process.env.PORT || 3000;
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+
+})
+
 
 app.post("/webhook", async (req, res) => {
     var id = (res.req.body.session).substr(43);
